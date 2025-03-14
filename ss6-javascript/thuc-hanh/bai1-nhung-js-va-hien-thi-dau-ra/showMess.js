306049e34c0456eb407f9d06d2db9f506e725bb1
Vahuki <kienhuu975@gmail.com>
2025-03-14 18:38:30 +0700

    update ss6

---
+++ ss6-javascript/thuc-hanh/bai1-nhung-js-va-hien-thi-dau-ra/showMess.js
@@ -0,0 +1,12 @@
+function showMessage(){
+    alert('Xin chào!');
+}
+function showMessage(){
+    console.log('Xin chào!');
+}
+function showMessage(){
+    document.writeln('Xin chào!');
+}
+function showMessage(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+}
