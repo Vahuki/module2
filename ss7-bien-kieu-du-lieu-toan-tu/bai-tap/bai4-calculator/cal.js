b5bcc70ce92c538ad95a25e796ec3db8d8fc9f30
Vahuki <kienhuu975@gmail.com>
2025-03-24 09:04:26 +0700

    tinh cong

--- ss7-bien-kieu-du-lieu-toan-tu/bai-tap/bai4-calculator/cal.js
+++ ss7-bien-kieu-du-lieu-toan-tu/bai-tap/bai4-calculator/cal.js
@@ -1,10 +1,11 @@
 
-document.querySelectorAll("input:not(.result)").forEach(button => {
-    button.addEventListener("click", function () {
-        let value = this.value; // Lấy giá trị của button
-        document.getElementById("output").value += value; // Nối giá trị vào ô output
-    });
-});
+
+function btnclick(value){
+    let x = document.getElementById('output').value;
+    x += value;
+    document.getElementById("output").value = x; // Nối giá trị vào ô output
+
+}
 
 function cal() {
     let result = document.getElementById("output").value;
